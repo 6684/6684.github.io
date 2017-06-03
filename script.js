2d2a1f14f719f45c2b975c52f2fdbfc2c7e27cf8
6684 <xyzzzzyx@ya.ru>
2017-06-03 18:42:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
     $('input').keydown(function(e) {
       if (e.which == 13){
-        $.post("https://quest-6684.herokuapp.com/", {password:$('input').val()}).done(function(data){
-          if (data != "fail"){
-            $('html, body').fadeOut('slow', function() {
-              window.location = data;
-          });
-          }
-          else{
-            $('div').fadeOut("fast").delay(1600).fadeIn('fast');
-            setTimeout(function(){
-              $("span").fadeIn('fast').delay(600).fadeOut('fast');
-              $('input').val("")
-          }, 600)
-
+        $.post("https://quest-6684.herokuapp.com/", {password:$('input').val().toLowerCase()}).done(function(data){
+            if(data != "Hello world!"){
+              if (data == "hint"){
+                $('div').fadeOut("fast").delay(2000).fadeIn('fast');
+                setTimeout(function(){
+                  $("a").fadeIn('fast').delay(1000).fadeOut('fast');
+                  $('input').val("")
+                }, 600)
+              }
+              else if (data == "fail"){
+                $('div').fadeOut("fast").delay(1600).fadeIn('fast');
+                setTimeout(function(){
+                  $("span").fadeIn('fast').delay(600).fadeOut('fast');
+                  $('input').val("")
+                }, 600)
+              }
+              else{
+                $('html, body').fadeOut('slow', function() {
+                  window.location = data;
+              });
+              }
           };
         });
       }
